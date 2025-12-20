868dbf4806ef6d8fe15671b81027a925e426046b
Lifetrace <neelovdenic@gmail.com>
2025-12-21 00:19:19 +0300

    New Structure and compiles

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(){
     glfwInit();
@@ -14,6 +15,7 @@ int main(){
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -21,16 +23,16 @@ int main(){
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-
+    
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     while (!glfwWindowShouldClose(window))
     {
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    
+
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
